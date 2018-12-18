d9b1e462139a6d5cd429091b18f28d33869a483b
Milan Oberkirch | geOps <milan.oberkirch@geops.de>
2018-12-18 18:15:47 +0100

    Tool to ingest VDV 451 data

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
-from __future__ import absolute_import
-
 import os
-
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -12,14 +9,14 @@ with open(os.path.join(here, 'requirements.txt')) as f:
                 if len(line) > 1 and not line.startswith('#')]
 
 setup(
-    name='vdv452db',
+    name='vdv451db',
     version='0.0.1',
-    description='Import VDV 542 files into a database',
+    description='Import VDV 451 files into a database',
     long_description=README,
     long_description_content_type='text/markdown',
     author='Milan Oberkirch | geOps',
     author_email='milan.oberkirch@geops.de',
-    keywords='vdv542 sql database',
+    keywords='vdv451 sql database',
     license='MIT',
     classifiers=[
         "Programming Language :: Python :: 2.7",
@@ -27,10 +24,11 @@ setup(
         "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
     ],
-    packages=['vdv452db'],
+    packages=['vdv451db'],
     install_requires=requires,
+    include_package_data=True,
     entry_points="""\
         [console_scripts]
-        vdv452db = vdv452db:main
+        vdv451db = vdv451db:main
     """,
   )
